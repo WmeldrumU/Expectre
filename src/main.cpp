f4ba3d85ddcc96f68517402e8a5cb088e9b88874
William Meldrum <William.Meldrum@motorolasolutions.com>
2022-07-26 23:55:45 -0600

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+﻿// Excpectre.cpp : Defines the entry point for the application.
+//
+#include <iostream>
+#include "spdlog.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "Hello Expectre" << endl;
+
+	spdlog::info("Welcome to spdlog!");
+
+	try {
+		//setup
+		while (true) {
+			//processInput
+			//update()
+			//render()
+		}
+	}
+	catch (exception &e) {
+		cout << "EXCEPTION: \n" << e.what() << endl;
+		return 1;
+	}
+	
+
+	return 0;
+}
