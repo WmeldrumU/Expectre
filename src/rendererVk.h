39d620cf804e1348f2caf41ea06963935608a26a
WmeldrumU <42564258+WmeldrumU@users.noreply.github.com>
2023-01-24 15:56:44 -0700

    create device and queue selection, change device selection method

--- src/rendererVk.h
+++ src/rendererVk.h
@@ -1,6 +1,10 @@
 #ifndef RENDERER_VK_H
 #define RENDERER_VK_H
 #include <vulkan/vulkan.h>
+#include <vector>
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_vulkan.h"
 
 namespace expectre {
     class Renderer_Vk {
@@ -9,12 +13,30 @@ namespace expectre {
         Renderer_Vk();
         ~Renderer_Vk();
 
-        bool enable_validation_layers{true};
+        bool m_enable_validation_layers{true};
 
     private:
         void create_instance();
+
+        void create_debug();
         
+        void create_surface();
+
+        void select_physical_device();
+
+        void select_queue_family();
+        void create_logical_device_and_queues();
+
+        SDL_Window* m_window{};
         VkInstance m_instance{};
+        VkSurfaceKHR m_surface{};
+        std::vector<const char*> m_validation_layers;
+        std::vector<VkPhysicalDevice> m_physical_devices;
+        VkDevice m_device;
+        uint32_t graphics_queue_family_index{UINT32_MAX};
+        uint32_t present_queue_family_index{UINT32_MAX};
+        VkQueue graphics_queue;
+        VkQueue present_queue;
     };
 }
 #endif // RENDERER_VK_H
--- src/shared.h
+++ src/shared.h
@@ -1,13 +1,14 @@
 #ifndef SHARED_H
 #define SHARED_H
-    
-    #define SCREEN_WIDTH 640
-    #define SCREEN_HEIGHT 480
+    namespace shared {
+        #define SCREEN_WIDTH 640
+        #define SCREEN_HEIGHT 480
 
-    #ifdef NDEBUG
-        constexpr bool validation_layers_enabled = false;
-    #else
-        constexpr bool validation_layers_enabled = true;
-    #endif // NDEBUG
+        #ifdef NDEBUG
+            constexpr bool validation_layers_enabled = false;
+        #else
+            constexpr bool validation_layers_enabled = true;
+        #endif // NDEBUG
 
+    }
 #endif // SHARED_H
