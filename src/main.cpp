f05ca9c0ae0e94c75f1985ca76940b3203fa2049
wmeldrum-glodon <william@glodonusa.com>
2023-11-18 12:35:59 -0700

    Work on validation layers

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 ﻿// Excpectre.cpp : Defines the entry point for the application.
 //
+#define NDEBUG
+
 #include <iostream>
 #include "SDL2/SDL.h"
 #include "Engine.h"
 #include "rendererVk.h"
+#include "spdlog/spdlog.h"
+
+
 
 int main(int argc, char *argv[])
 {
+	spdlog::set_level(spdlog::level::debug);
 	try
 	{
 		std::cout << "STARTING UP...." << std::endl;
