71257c3cea62fcb69830bb4fa652086c6119e542
wmeldrum-glodon <william@glodonusa.com>
2023-11-25 18:43:45 -0700

    Add VkImage and buffer creation

--- src/rendererVk.cpp
+++ src/rendererVk.cpp
@@ -42,16 +42,18 @@ namespace Expectre
             SDL_Log("Unable to initialize application window!: %s", SDL_GetError());
             throw std::runtime_error("Unable to initialize application window!");
         }
-#ifndef NDEBUG
-        enable_layers();
-#endif
+
+        enumerate_layers();
+
         create_instance();
 
         // cnumeratePhysicalDevices();
 
         select_physical_device();
 
-        create_logical_device_and_queues(); // problem function
+        create_logical_device_and_queues(); 
+
+        create_buffers_and_images();
 
         // createSemaphores();
 
@@ -74,11 +76,16 @@ namespace Expectre
         // renderProgManager.Init();
 
         // vertexCache.Init(... );
+        // cleanup();
     }
 
     Renderer_Vk::~Renderer_Vk()
     {
-        vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
+        // vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
+        vkDeviceWaitIdle(m_device);
+        vkDestroyImage(m_device, m_image, nullptr);
+        vkDestroyBuffer(m_device, m_buffer, nullptr);
+        vkDestroyDevice(m_device, nullptr);
         vkDestroyInstance(m_instance, nullptr);
         SDL_DestroyWindow(m_window);
         m_window = nullptr;
@@ -111,12 +118,25 @@ namespace Expectre
         instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         instance_create_info.pNext = nullptr;
         instance_create_info.pApplicationInfo = &app_info; // create_info depends on app_info
-        instance_create_info.enabledLayerCount = 0;
+        uint32_t layer_count = 0;
+        const char **p_layers = nullptr;
+        if (m_layers_supported)
+        {
+            layer_count = static_cast<uint32_t>(m_layers.size());
+            p_layers = m_layers.data();
+        }
+        instance_create_info.enabledLayerCount = layer_count;
+        instance_create_info.ppEnabledLayerNames = p_layers;
 
-        vkCreateInstance(&instance_create_info, nullptr, &m_instance);
+        auto result = vkCreateInstance(&instance_create_info, nullptr, &m_instance);
+
+        if (result != VK_SUCCESS)
+        {
+            spdlog::error("Could not create instance.");
+        }
     }
 
-    void Renderer_Vk::enable_layers()
+    void Renderer_Vk::enumerate_layers()
     {
 
         uint32_t count = 0;
@@ -127,31 +147,27 @@ namespace Expectre
             return;
         }
         VkResult result = vkEnumerateInstanceLayerProperties(&count, properties.data());
-        bool found = false;
+
         for (const auto &property : properties)
         {
-            if (strcmp(property.layerName,"VK_LAYER_KHRONOS_validation") == 0)
+            for (const auto &layer_name : m_layers)
             {
-                found = true;
-                spdlog::debug("Validation layers found!");
-                break;
+                if (strcmp(property.layerName, layer_name) == 0)
+                {
+                    m_layers_supported = true;
+                    spdlog::debug("Validation layers found!");
+                    return;
+                }
             }
-            spdlog::debug(property.layerName);
-            spdlog::debug(property.description);
+
+            // spdlog::debug(property.layerName);
+            // spdlog::debug(property.description);
         }
-        if (!found)
+        if (!m_layers_supported)
         {
             spdlog::debug("Validation layers NOT found!");
         }
     }
-
-    // void Renderer_Vk::create_debug() {
-    //     PFN_vkCreateDebugReportCallbackEXT SDL2_vkCreateDebugReportCallbackEXT = nullptr;
-    //     VkDebugReportCallbackCreateInfoEXT debug_callback_create_info {};
-    //     debug_callback_create_info.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
-    //     debug_callback_create_info.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIG_EXT;
-    //     debug_callback_create_info.pfnCallback = //some function
-    // }
 
     void Renderer_Vk::create_surface()
     {
@@ -206,42 +222,6 @@ namespace Expectre
         assert(m_chosen_phys_device.has_value());
     }
 
-    // void Renderer_Vk::select_physical_device()
-    // {
-
-    //     uint32_t queue_family_count;
-
-    //     vkGetPhysicalDeviceQueueFamilyProperties(m_physical_devices[0], &queue_family_count, nullptr);
-    //     std::vector<VkQueueFamilyProperties> queue_family_properties(queue_family_count);
-    //     vkGetPhysicalDeviceQueueFamilyProperties(m_physical_devices[0], &queue_family_count, queue_family_properties.data());
-
-    //     int32_t graphic_index = -1;
-    //     int32_t present_index = -1;
-
-    //     int32_t i = 0;
-    //     for (const auto &queue_family : queue_family_properties)
-    //     {
-    //         if (queue_family.queueCount > 0 && queue_family.queueFlags & VK_QUEUE_GRAPHICS_BIT)
-    //         {
-    //             graphic_index = i;
-    //         }
-
-    //         VkBool32 present_support = false;
-    //         vkGetPhysicalDeviceSurfaceSupportKHR(m_physical_devices[0], i, m_surface, &present_support);
-    //         if (queue_family.queueCount > 0 && present_support)
-    //         {
-    //             present_index = i;
-    //         }
-
-    //         if (graphic_index != -1 && present_index != -1)
-    //         {
-    //             break; // found queue(s) for graphics and present functionality
-    //         }
-
-    //         i++;
-    //     }
-    // }
-
     void Renderer_Vk::create_logical_device_and_queues()
     {
         // Queue family logic
@@ -272,12 +252,12 @@ namespace Expectre
         queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
         queue_create_info.flags = NULL;
         queue_create_info.queueCount = 1;
-        queue_create_info.pQueuePriorities = nullptr;
+        queue_create_info.pQueuePriorities = &m_priority;
         queue_create_info.queueFamilyIndex = family_index;
 
-        VkPhysicalDeviceFeatures supportedFeatures;
-        VkPhysicalDeviceFeatures requiredFeatures = {};
-
+        VkPhysicalDeviceFeatures supportedFeatures{};
+        vkGetPhysicalDeviceFeatures(m_chosen_phys_device.value(), &supportedFeatures);
+        VkPhysicalDeviceFeatures requiredFeatures{};
         requiredFeatures.multiDrawIndirect = supportedFeatures.multiDrawIndirect;
         requiredFeatures.tessellationShader = VK_TRUE;
         requiredFeatures.geometryShader = VK_TRUE;
@@ -294,7 +274,43 @@ namespace Expectre
             spdlog::throw_spdlog_ex("Could not create logical device");
         }
     }
-    void Renderer_Vk::cleanup()
+
+    void Renderer_Vk::create_buffers_and_images()
     {
+        VkBufferCreateInfo buffer_info{};
+        buffer_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        buffer_info.size = 1024 * 1024 * 512; // 512MB
+        buffer_info.pNext = nullptr;
+        buffer_info.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+        buffer_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+        if (vkCreateBuffer(m_device, &buffer_info, nullptr, &m_buffer) != VK_SUCCESS)
+        {
+            spdlog::throw_spdlog_ex("Could not create buffer");
+        }
+
+        VkFormat format{};
+        VkFormatProperties properties{};
+        vkGetPhysicalDeviceFormatProperties(m_chosen_phys_device.value(), format, &properties);
+
+        // vkGetPhysicalDeviceImageFormatProperties(m_device, )
+
+        VkImageCreateInfo image_info{};
+        image_info.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+        image_info.imageType = VK_IMAGE_TYPE_2D;
+        image_info.format = VK_FORMAT_R8G8B8A8_UNORM;
+        image_info.extent = {1024, 1024, 1};
+        image_info.mipLevels = 10;
+        image_info.arrayLayers = 1;
+        image_info.tiling = VK_IMAGE_TILING_OPTIMAL;
+        image_info.samples = VK_SAMPLE_COUNT_1_BIT;
+        image_info.usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+        image_info.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        image_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+        if (vkCreateImage(m_device, &image_info, nullptr, &m_image) != VK_SUCCESS)
+        {
+            spdlog::throw_spdlog_ex("Could not create image.");
+        }
     }
 }
