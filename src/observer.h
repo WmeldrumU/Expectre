373a96fd71e27924a9a74999ecc3711b892a7ce7
WmeldrumU <wmeldrum1@gmail.com>
2026-03-07 19:22:40 -0700

    wasd movement, it's broken

---
+++ src/observer.h
@@ -0,0 +1,15 @@
+#ifndef EXPECTRE_OBSERVER_H
+#define EXPECTRE_OBSERVER_H
+#include <SDL2/SDL.h>
+
+namespace Expectre
+{
+
+    class InputObserver
+    {
+    public:
+        virtual ~InputObserver() = default;
+        virtual void on_input_event(const SDL_Event &event) = 0;
+    };
+}
+#endif
