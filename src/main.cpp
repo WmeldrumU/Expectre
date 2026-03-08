048341a15569ef6c36c2357e907a47f6b7efec7b
WmeldrumU <wmeldrum1@gmail.com>
2026-03-07 19:21:45 -0700

    Revert "update"
    This reverts commit 229397973393b571ce829adf32a6723d638a51ec.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 ﻿// Excpectre.cpp : Defines the entry point for the application.
 //
 #include <iostream>
-#include <vulkan/vulkan.h>
 //#include <chrono.h>
 
 #include "spdlog/spdlog.h"
 #include "SDL2/SDL.h"
-#include "SDL2/SDL_vulkan.h"
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -16,8 +14,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	//chrono::milliseconds engineClock(1000);
-	VkExtent2D _windowExtent{1700, 900};
-	
+
 	cout << "Hello Expectre" << endl;
 	
 	spdlog::info("Welcome to spdlog!");
@@ -39,15 +36,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	screenSurface = SDL_GetWindowSurface(window);
-	SDL_Rect rect1;
-	rect1.h = 200;
-	rect1.w = 200;
-	rect1.x = 50;
-	rect1.y = 100;
-	
-	
 	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-	SDL_FillRect(screenSurface, &rect1, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
 	SDL_UpdateWindowSurface(window);
 	SDL_Delay(3000);
 	SDL_DestroyWindow(window);
