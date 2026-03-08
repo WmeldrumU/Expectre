41614b01d94bc8000d07e75e92c24d2d2dfdeb74
WmeldrumU <wmeldrum1@gmail.com>
2026-03-07 19:21:47 -0700

    sdl fixing

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Expectre
+# Install script for directory: C:/Users/meldr/source/repos/Expectre
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Expectre/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/meldr/source/repos/Expectre/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
